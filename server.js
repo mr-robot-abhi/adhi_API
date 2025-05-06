edc313c43ce6874d51e05f5abe96b69d6b15d4bc
Abhishek Math <41338114+mr-robot-abhi@users.noreply.github.com>
2025-05-06 13:20:53 +0530

    Update server.js
    MONGO URI Fix

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect(process.env.DB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
