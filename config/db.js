cf071d2b76a9e47f623988866f96eba211912d86
Abhishek Math <41338114+mr-robot-abhi@users.noreply.github.com>
2025-05-07 15:01:13 +0530

    Update db.js
    Removed the deprecated warnings for MongoDB

--- config/db.js
+++ config/db.js
@@ -1,13 +1,9 @@
-// config/db.js
 const mongoose = require('mongoose');
-const logger = require('../utils/logger'); // Updated path
+const logger = require('../utils/logger');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     logger.info('✅ MongoDB connected');
   } catch (error) {
     logger.error(`❌ MongoDB connection failed: ${error.message}`);
